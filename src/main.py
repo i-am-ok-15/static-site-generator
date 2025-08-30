cca2fa971bfbb9c5276f08043bb0919a0d097a7b
rtasker15 <rtasker15@gmail.com>
2025-08-30 12:25:32 +0100

    ssg, ch2 l1, complete

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from textnode import TextNode
 
 
 def main():
-    text_1 = TextNode("hello", "plain", "hello.com")
-
-    print(text_1)
-
+    text_1 = TextNode("anchor text", "text type", "some url")
+    print(f"TextNode({text_1.text}, {text_1.text_type}, {text_1.url})")
 main()
